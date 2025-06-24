1d668658a9b127b96d7134090bc5518a716ba24d
Kareem-Bizreh <kareembezreh@gmail.com>
2025-06-24 14:05:40 +0300

    initialize physics classes

---
+++ src/physics/Physics.js
@@ -0,0 +1,5 @@
+import { Vector3 } from "three";
+
+export default class Physics {
+  constructor() {}
+}
---
+++ src/physics/ThrustForce.js
@@ -0,0 +1,6 @@
+import { Vector3 } from "three";
+import Force from "./Force";
+
+export default class ThrustForce extends Force {
+  constructor() {}
+}
