86cc9b61d150c989de79747ccb0c32ea40d7b5b1
OnlyAbdullh <abdallaalkasm9@gmail.com>
2025-06-26 18:15:19 +0300

    creating Engine and Thrust Force classes with its methods and using one Singleton

--- src/physics/Engine.js
+++ src/physics/Engine.js
@@ -1,5 +1,46 @@
-import { Vector3 } from "three";
+import Fuel from './Fuel.js';
+import ThrustForce from './ThrustForce.js';
+import { Vector3 } from 'three';
 
 export default class Engine {
-  constructor() {}
+  constructor(initialFuelMass, fuelTypeName, env) {
+    this.fuel = new Fuel(initialFuelMass, fuelTypeName);
+    this.env = env;
+
+    this.nominalBurnRate = 12890;
+    this.thrustTime = 200;
+    this.nozzleCount = 5;
+
+    this.thrustForce = new ThrustForce(this.fuel, this.env);
+    this.thrustVector = new Vector3();
+    this.isActive = false;
+    this.elapsedTime = 0;
+  }
+
+  start() {
+    this.isActive = true;
+    this.elapsedTime = 0;
+  }
+
+  stop() {
+    this.isActive = false;
+  }
+
+  isBurning() {
+    return this.isActive && this.elapsedTime < this.thrustTime;
+  }
+
+  update(deltaTime) {
+    if (!this.isBurning()) {
+      this.isActive = false;
+      this.thrustVector.set(0, 0, 0);
+      return;
+    }
+
+    this.fuel.update(deltaTime, this.nominalBurnRate);
+    this.thrustForce.update();
+
+    this.thrustVector.copy(this.thrustForce.force).multiplyScalar(this.nozzleCount);
+    this.elapsedTime += deltaTime;
+  }
 }
--- src/physics/ThrustForce.js
+++ src/physics/ThrustForce.js
@@ -1,6 +1,54 @@
-import { Vector3 } from "three";
-import Force from "./Force";
+// ThrustForce.js
+import Force from './Force.js';
+import Rocket from './Rocket.js';
+import Environment from './Environment.js';
+import { Vector3 } from 'three';
+
+let instance = null;
 
 export default class ThrustForce extends Force {
-  constructor() {}
+  constructor() {
+    if (instance) return instance;
+    super();
+    instance = this;
+
+    this.rocket = new Rocket();
+    this.env = new Environment();
+
+    this.A_throat = 8.0;
+    this.A_exit = 4.55;
+    this.chamberPressure = 6.8e5;
+
+    this.update();
+  }
+
+  _computeMassFlowRate() {
+    const { specificHeatRatio: γ, gasConstant: Ru, chamberTemperature: Tt, molecularWeight: M } = this.rocket.fuel.type;
+    const R_spec = (Ru / M) * 1000;  
+    const Pt = this.chamberPressure;
+    const term1 = this.A_throat * Pt / Math.sqrt(Tt);
+    const term2 = Math.sqrt(γ / R_spec);
+    const term3 = Math.pow((γ + 1) / 2, - (γ + 1) / (2 * (γ - 1)));
+    return term1 * term2 * term3;
+  }
+
+  _computeIdealVe() {
+    const { specificHeatRatio: γ, gasConstant: Ru, chamberTemperature: Tt, molecularWeight: M } = this.rocket.fuel.type;
+    const R_spec = (Ru / M) * 1000;
+    const pe = this.env.pressure;
+    const Pt = this.chamberPressure;
+    const exp = (γ - 1) / γ;
+    return Math.sqrt((2 * γ) / (γ - 1) * R_spec * Tt * (1 - Math.pow(pe / Pt, exp)));
+  }
+
+  update() {
+    const mDot = this._computeMassFlowRate();
+    const veIdeal = this._computeIdealVe();
+    const pe = this.env.pressure;
+    const p0 = this.env.seaLevelpressure;
+    const veEff = veIdeal + ((p0 - pe) * this.A_exit) / mDot;
+
+    const thrust = mDot * veEff;
+    this.force.copy(new Vector3(0, thrust, 0));
+  }
 }
