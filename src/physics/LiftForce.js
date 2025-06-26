b5d76415f7f703a24b8c1e79d6f944531e34eec5
Kareem-Bizreh <kareembezreh@gmail.com>
2025-06-26 12:20:45 +0300

    Create Lift Force

--- src/physics/LiftForce.js
+++ src/physics/LiftForce.js
@@ -1,6 +1,35 @@
-import { Vector3 } from "three";
 import Force from "./Force";
 
+let instance = null;
 export default class LiftForce extends Force {
-  constructor() {}
+  constructor() {
+    if (instance) {
+      return instance;
+    }
+    instance = this;
+
+    super();
+    this.environment = new Environment();
+    this.rocket = new Rocket();
+
+    this.update();
+  }
+
+  update() {
+    const velocityDir = this.rocket.velocity.clone().normalize();
+    const speed = this.rocket.velocity.length();
+
+    const liftDir = new Vector3(-velocityDir.y, velocityDir.x, 0).normalize();
+
+    const liftMagnitude =
+      this.rocket.liftCoefficient *
+      this.rocket.A *
+      0.5 *
+      this.environment.airDensity *
+      Math.pow(speed, 2);
+
+    const liftForce = liftDir.clone().multiplyScalar(liftMagnitude);
+
+    this.force.copy(liftForce);
+  }
 }
